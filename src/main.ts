9d02f4506f06ea3e1def2966ed89855e96ea29b4
Jack <jack13only@gmail.com>
2022-10-29 20:31:29 +0300

    feat: use port from env

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import serverlessExpress from '@vendia/serverless-express'
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
-import * as helmet from 'helmet';
+import helmet from 'helmet';
 
 let server: Handler;
 
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-
-//   app.enableCors({
-//     origin: (req, callback) => callback(null, true),
-//   });
-//   app.use(helmet());
-
-//   await app.listen(port);
-// }
-// bootstrap().then(() => {
-//   console.log('App is running on %s port', port);
-// });
   const app = await NestFactory.create(AppModule);
   await app.init();
+
+  app.enableCors({
+    origin: (req, callback) => callback(null, true),
+  });
+  app.use(helmet());
+
+  await app.listen(port);
 
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
